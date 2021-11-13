25c5ce8c46665cafb6b06ea5f0b4ef68061c4c95
KaterynaTrofimyuk <73081823+Katrin18@users.noreply.github.com>
2021-11-13 09:03:29 +0200

    lesson-1

---
+++ lesson-1/classwork/main.js
@@ -0,0 +1,61 @@
+//
+// 1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
+
+// let arr=[10,15,12,7,9,52,3,85,1,7];
+//
+// let result=arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5]+arr[6]+arr[7]+arr[8]+arr[9];
+// console.log(result);
+
+// 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+
+// let book={
+//     name:'Book',
+//     count:359,
+//     genre:'rubai'
+// }
+// 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
+
+// let book={
+//     name:'Book',
+//     count:201,
+//     genre:'drama',
+//     authors:['Janiy Lokren', 'Ballor Vakol', 'Lorin Samten']
+// }
+
+// 4. Створити маис об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори.
+// Вивести кожну книгу як окремий об'єкт
+
+// let books=[
+//     {name:'Book1',count:208,genre:'drama',authors:['Hoklert Moiy','Uoli Jute']},
+//     {name:'Book2',count:407,genre:'comedy',authors:['Santer Mer','Momik Garester']},
+//     {name:'Book3',count:968,genre:'drama',authors:['Aloper Vaser','Kasti Krol']}
+// ]
+// console.log(books[0]);
+// console.log(books[1]);
+// console.log(books[2]);
+
+// 5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+// Значення площі зберігати в змінній s.
+
+// let height=23;
+// let width=10;
+// let s=height*width;
+// console.log(s);
+
+// 6. Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
+// результат помістіть у змінну v.
+
+// let heightC=10;
+// let dC=4;
+// const P=3.14;
+// let v=P*Math.pow((dC/2),2)*heightC;
+// console.log(v);
+
+// 7. У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
+// Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь)
+// або оператор зведення в ступінь **).
+
+// let n=3;
+// let m=4;
+// let k = Math.sqrt(Math.pow(n,2) + Math.pow(m,2));
+// console.log(k)
---
+++ lesson-1/homework/main.js
@@ -0,0 +1,203 @@
+//  Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7,
+// 16, true, false.Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// let hello='hello';
+// let owu='owu';
+// let ua='ua';
+// let a=1;
+// let b=10;
+// let c=-999;
+// let d=123;
+// let e=3.14;
+// let f=2.7;
+// let k=16;
+// let g=true;
+// let s=false;
+//
+//
+// console.log(hello);
+// alert(hello);
+// document.write(hello,'<br>');
+//
+// console.log(owu);
+// alert(owu);
+// document.write(owu,'<br>');
+//
+// console.log(ua);
+// alert(ua);
+// document.write(ua,'<br>');
+//
+//
+// console.log(a);
+// alert(a);
+// document.write(a,'<br>');
+//
+//
+// console.log(b);
+// alert(b);
+// document.write(b,'<br>');
+//
+// console.log(c);
+// alert(c);
+// document.write(c,'<br>');
+//
+//
+// console.log(d);
+// alert(d);
+// document.write(d,'<br>');
+//
+//
+// console.log(e);
+// alert(e);
+// document.write(e,'<br>');
+//
+// console.log(f);
+// alert(f);
+// document.write(f),'<br>';
+//
+// console.log(k);
+// alert(k);
+// document.write(k,'<br>');
+//
+// console.log(g);
+// alert(g);
+// document.write(g,'<br>');
+//
+//
+// console.log(s);
+// alert(s);
+// document.write(s);
+// document.write('<br>','<hr>');
+
+//- Переприсвоїти кожній змінній з завдання значення на довільне.
+//  Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+//  hello='HELLO';
+//  owu='OWU';
+//  ua='UA';
+//  a=15;
+//  b=32;
+//  c=-101;
+//  d=321;
+//  e=10.1;
+//  f=8.8;
+//  k=4;
+//  g=false;
+// s=true;
+//
+//
+// console.log(hello);
+// alert(hello);
+// document.write(hello,'<br>');
+//
+// console.log(owu);
+// alert(owu);
+// document.write(owu,'<br>');
+//
+// console.log(ua);
+// alert(ua);
+// document.write(ua,'<br>');
+//
+//
+// console.log(a);
+// alert(a);
+// document.write(a,'<br>');
+//
+//
+// console.log(b);
+// alert(b);
+// document.write(b,'<br>');
+//
+// console.log(c);
+// alert(c);
+// document.write(c,'<br>');
+//
+//
+// console.log(d);
+// alert(d);
+// document.write(d,'<br>');
+//
+//
+// console.log(e);
+// alert(e);
+// document.write(e,'<br>');
+//
+// console.log(f);
+// alert(f);
+// document.write(f),'<br>';
+//
+// console.log(k);
+// alert(k);
+// document.write(k,'<br>');
+//
+// console.log(g);
+// alert(g);
+// document.write(g,'<br>');
+//
+//
+// console.log(s);
+// alert(s);
+// document.write(s);
+// document.write('<br>','<hr>');
+
+
+//- Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+
+// let firstName='Kateryna';
+// let lastName='Trofymiuk';
+// let middleName='Oleksandrivna'
+
+// let person=firstName+lastName+middleName;
+// console.log(person);
+
+
+//- За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//  Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+// let firstName=prompt('What is first name?');
+// let middleName=prompt('What is middle name?');
+// let age=prompt('How old are you?');
+// document.write('Вітаю '+firstName+' '+middleName+'. Тобі '+age+' років');
+
+
+
+//- За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//  let a = 100; let b = '100'; let c = true;
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+
+//- Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//  В однакових виразах не використовувати однакові оператори!!!
+
+//  5 ? 6 -> true
+//  5 ? 6 -> false
+//  5 ? 6 -> false
+//  5 ? 6 -> false
+//  10 ? 10 -> true
+//  10 ? 10 -> true
+//  10 ? 10 -> false
+//  10 ? 10 -> false
+//  10 ? 10 -> false
+//  123 ? '123' -> false
+//  123 ? '123' -> true
+
+// let a1=5 < 6 ;
+// let a2=5 > 6;
+// let a3=(5 == 6) ;
+// let a4=5 === 6 ;
+// let a5=10 == 10 ;
+// let a6=10 ===10 ;
+// let a7=10 < 10;
+// let a8=10 > 10 ;
+// let a9=10 !== 10 ;
+// let a10=123 ==='123' ;
+// let a11=  123 =='123';
+// console.log(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11);
+
